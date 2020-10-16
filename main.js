4aecf06e31b71bc0fafca3a14d1282f00c7f46a0
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2020-10-16 09:22:06 -0700

    completed portfolio

--- main.js
+++ main.js
@@ -40,9 +40,9 @@ sr.reveal(".home-img", { delay: 400 });
 sr.reveal(".home-social-icon", { interval: 200 });
 
 /*SCROLL ABOUT*/
-sr.reveal(".about-img", {});
 sr.reveal(".about-subtitle", { delay: 400 });
 sr.reveal(".about-text", { delay: 400 });
+sr.reveal(".about-img", { delay: 600 });
 
 /*SCROLL SKILLS*/
 sr.reveal(".skills-subtitle", {});
@@ -51,7 +51,63 @@ sr.reveal(".skills-data", { interval: 200 });
 sr.reveal(".skills-img", { delay: 600 });
 
 /*SCROLL WORK*/
+sr.reveal(".section-title", { delay: 400 });
 sr.reveal(".work-img", { interval: 200 });
+sr.reveal(".work-text", { delay: 600 });
 
 /*SCROLL CONTACT*/
 sr.reveal(".contact-input", { interval: 200 });
+
+/*WORK OVERLAYS*/
+function onOne() {
+  document.querySelector("#overlay-one").style.display = "block";
+}
+function offOne() {
+  document.querySelector("#overlay-one").style.display = "none";
+}
+function onTwo() {
+  document.querySelector("#overlay-two").style.display = "block";
+}
+function offTwo() {
+  document.querySelector("#overlay-two").style.display = "none";
+}
+function onThree() {
+  document.querySelector("#overlay-three").style.display = "block";
+}
+function offThree() {
+  document.querySelector("#overlay-three").style.display = "none";
+}
+function onFour() {
+  document.querySelector("#overlay-four").style.display = "block";
+}
+function offFour() {
+  document.querySelector("#overlay-four").style.display = "none";
+}
+function onFive() {
+  document.querySelector("#overlay-five").style.display = "block";
+}
+function offFive() {
+  document.querySelector("#overlay-five").style.display = "none";
+}
+function onSix() {
+  document.querySelector("#overlay-six").style.display = "block";
+}
+function offSix() {
+  document.querySelector("#overlay-six").style.display = "none";
+}
+
+const title = document.getElementById('title');
+
+const typewriter = new Typewriter(title, {
+    loop: true
+});
+
+typewriter.typeString('WEB DEVELOPER')
+    .pauseFor(1000)
+    .deleteChars(9)
+    .typeString('DESIGNER')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('CREATOR')
+    .pauseFor(1000)
+    .start();
